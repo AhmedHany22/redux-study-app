0c1221c44e3f47d6468448baeb1099f2fb12fc20
AmdHany22 <ahmed_hany19@outlook.com>
2021-11-01 15:45:25 +0200

    DIspatch Actions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import store from "./store";
+
+const unsubscribe = store.subscribe(() =>
+  console.log("State changed :", store.getState())
+);
+
+store.dispatch({ type: "bugAdded", description: "Bug 1" });
+store.dispatch({ type: "bugAdded", description: "Bug 2" });
+unsubscribe();
+store.dispatch({ type: "bugRemoved", id: 1 });
