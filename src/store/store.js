e1e7d6491d364b74c058bfa9b70f45efdbee1012
AmdHany22 <ahmed_hany19@outlook.com>
2021-11-01 20:02:46 +0200

    Using the Reduxjs/Toolkit to create Store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,4 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./bugs";
 
-const store = createStore(reducer, devToolsEnhancer({ trace: true }));
-
-export default store;
+export default configureStore({ reducer });
