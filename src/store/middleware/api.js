b18d478b924ad8ff7086466ab9fc377270c53270
AmdHany22 <ahmed_hany19@outlook.com>
2021-11-05 00:43:24 +0200

    Solving the api and the success dispatch

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { apiCallBegain, apiCallFailed } from "../apiActions";
+import { apiCallBegain, apiCallFailed, apiCallSuccess } from "../apiActions";
 
 const api = (store) => (next) => async (action) => {
   if (action.type !== apiCallBegain.type) return next(action);
@@ -8,12 +8,13 @@ const api = (store) => (next) => async (action) => {
   const { url, method, data, onSuccess, onFailure } = action.payload;
   try {
     const response = await axios.request({
-      baseURL: "http://localhost:9002/api",
+      baseURL: "http://localhost:9001/api",
       url,
       method,
       data,
     });
-    store.dispatch({ type: onSuccess, payload: response.data });
+    store.dispatch(apiCallSuccess(response.data));
+    if (onSuccess) store.dispatch({ type: onSuccess, payload: response.data });
   } catch (error) {
     store.dispatch(apiCallFailed(error));
     if (onFailure) store.dispatch({ type: onFailure, payload: error.message });
